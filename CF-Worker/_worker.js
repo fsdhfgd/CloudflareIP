98faaa0989ef3869775ed1b3b4a449c9c30748ce
fsdhfgd <ruwu0102@gmail.com>
2026-02-10 09:36:16 +0800

    Update UUID for 我的VL密钥

--- CF-Worker/_worker.js
+++ CF-Worker/_worker.js
@@ -6,7 +6,7 @@
 
 import { connect } from 'cloudflare:sockets';
 
-let 我的VL密钥 = '04c808e2-0b59-47b0-a54b-32fc7ef1c902';//UUID
+let 我的VL密钥 = '9db63ede-d18d-4cab-b44c-3e1717d03443';//UUID
 let 反代IP = 'proxyip.cmliussss.net'; //反代IP
 
 export default {
